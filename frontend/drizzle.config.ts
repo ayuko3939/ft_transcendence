99f97e80ccd3a2751f8a1d116c63c5bcbb62991f
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-05-01 00:11:28 +0900

    add: databaseのスキーマも含めたファイルを追加

---
+++ frontend/drizzle.config.ts
@@ -0,0 +1,12 @@
+// import "dotenv/config";
+
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/app/api/database/schema.ts",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: process.env.DB_FILE_NAME ?? "file:../database/database.db",
+  },
+});
