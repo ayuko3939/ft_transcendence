421a799d0a420d2af6d2769c30cb8fef45601722
myusername <myusername@myemaildomain.com>
2025-05-02 18:22:41 +0900

    update: 暫定的にfrontendをdocker-composeで動かせるようにしたが、databaseの永続化ができていない。

--- frontend/src/app/api/database/db.ts
+++ frontend/src/app/api/database/db.ts
@@ -1,8 +1,16 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import path from "node:path";
+
+const databaseFileName =
+  "file:" +
+  path.join(
+    process.env.DB_FILE_DIR ?? "../database",
+    process.env.DB_FILE_NAME ?? "database.db"
+  );
 
 const client = createClient({
-  url: process.env.DB_FILE_NAME ?? "file:../database/database.db",
+  url: process.env.NEXT_BUILD === 'true' ? "file:./dummy.db" : databaseFileName,
 });
 
 export const db = drizzle(client, { logger: true });
