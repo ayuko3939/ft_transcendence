019e8b95f80939fd313aa4532d5d8d2d8801d945
gawingowin <101625248+GawinGowin@users.noreply.github.com>
2025-05-17 18:02:20 +0900

    update(ui): game中に離脱しようとしたら、警告っぽいのが出るようにした。 (#36)
    * add: ゲームの中断に関しての確認メッセージを追加
    
    * fmt
    
    * update: /gameに保護を追加
    
    ---------
    
    Co-authored-by: myusername <myusername@myemaildomain.com>

--- frontend/src/app/(authed)/game/page.tsx
+++ frontend/src/app/(authed)/game/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 // SSRを無効化したコンポーネントの読み込み
 const CanvasComponent = dynamic(() => import("./_components/PongGame"), {
@@ -8,6 +11,56 @@ const CanvasComponent = dynamic(() => import("./_components/PongGame"), {
 });
 
 export default function Game() {
+  const { status } = useSession();
+  const router = useRouter();
+  const [isGameReady, setIsGameReady] = useState(true);
+
+  useEffect(() => {
+    window.history.pushState(null, "", window.location.pathname);
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      const message = "ゲームをプレイ中です。このページを離れますか？";
+      (e || window.event).returnValue = message;
+      return message;
+    };
+
+    const handlePopState = () => {
+      if (isGameReady) {
+        const confirmLeave = confirm(
+          "ゲームをプレイ中です。このページを離れますか？",
+        );
+        if (!confirmLeave) {
+          window.history.pushState(null, "", window.location.pathname);
+        }
+      }
+    };
+
+    if (isGameReady) {
+      window.addEventListener("beforeunload", handleBeforeUnload);
+      window.addEventListener("popstate", handlePopState);
+    }
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, [isGameReady]);
+
+  const handleGameReady = () => {
+    setIsGameReady(false);
+  };
+
+  if (status === "unauthenticated") {
+    router.push("/login");
+    return null;
+  }
+  if (status === "loading") {
+    return (
+      <div className="relative z-5 flex min-h-screen items-center justify-center overflow-hidden">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
     <div className="grid min-h-screen place-items-center">
       <main>
