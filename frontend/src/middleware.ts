34fff394e5bde8f5fef5c4ad948daddb4a581f0a
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-06-05 22:59:31 +0900

    hotfix: ,middlewareでの設定ミスを修正

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -64,7 +64,7 @@ export function middleware(req: NextRequest) {
     return NextResponse.rewrite(tournamentWsUrl);
   }
 
-  if (req.nextUrl.pathname.startsWith("/ws/game/")) {
+  if (req.nextUrl.pathname.startsWith("/ws/game")) {
     const onlineWsUrl = joinWsUrl(backendWsUrl, "game");
     console.log("オンライン対戦用WebSocketプロキシ:", onlineWsUrl);
     return NextResponse.rewrite(onlineWsUrl);
