c537ce3f8e148b029da3fdde1ba2f40b0e7d3380
GawinGowin <saraki@student.42tokyo.jp>
2025-06-07 22:54:57 +0900

    real domain settiongs

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -13,8 +13,8 @@ if [ ! -f /etc/nginx/ssl/nginx.crt ] || [ ! -f /etc/nginx/ssl/nginx.key ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
             -keyout /etc/nginx/ssl/nginx.key \
             -out /etc/nginx/ssl/nginx.crt \
-            -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=localhost" \
-            -addext "subjectAltName=DNS:localhost,IP:127.0.0.1"
+            -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=ft-transcendence.gawingowin.systems" \
+            -addext "subjectAltName=DNS:ft-transcendence.gawingowin.systems,IP:10.12.3.9"
     chmod 644 /etc/nginx/ssl/nginx.key
     echo "SSL certificate generated."
 fi
